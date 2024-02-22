923e8806c13376f660307b717b700dc17d84b423
sire-ambrose <ikpeleambroseobinna@gmail.com>
2024-02-22 14:05:04 +0100

    tradingview added

--- ngnmarket/market.py
+++ ngnmarket/market.py
@@ -4,6 +4,7 @@ from datetime import datetime
 import re
 from bs4 import BeautifulSoup
 import json
+from selenium import webdriver
 
 
 class Stock:
@@ -21,7 +22,7 @@ class Stock:
         df= pd.DataFrame(date_price_list, columns=['Date', 'Price'])
         return df
     
-    def getprice_experimental(self):
+    def get_price_experimental(self, start, end):
         
         lower_ticker= self.ticker.lower()
         r= requests.get(f'https://www.investing.com/search/?q={lower_ticker}')
@@ -44,12 +45,28 @@ class Stock:
             a,b=i.split("=")
             res[a]=b
 
-        data_link='https://tvc4.investing.com/'+res['carrier']+'/'+res['time']+'/'+res['domain_ID']+'/'+res['lang_ID']+'/'+res['timezone_ID']+'/'+'history'+'?'+'symbol='+res['pair_ID']+'&'+'resolution=D&from=1076920568&to=1705479045'
+        # Convert the date string to a datetime object
+        start = str(int(datetime.strptime(start, '%d-%m-%Y').timestamp() ))
+        end = str(int(datetime.strptime(end, '%d-%m-%Y').timestamp()))
 
+        data_link='https://tvc4.investing.com/'+res['carrier']+'/'+res['time']+'/'+res['domain_ID']+'/'+res['lang_ID']+'/'+res['timezone_ID']+'/'+'history'+'?'+'symbol='+res['pair_ID']+'&'+'resolution=D&from='+start+'&to='+end
+        print(data_link)
+        driver = webdriver.Chrome()
+        driver.get(data_link)
+        #https://chromedriver.storage.googleapis.com/index.html?path=79.0.3945.36/
+
+        r= driver.page_source
+        soup= BeautifulSoup(r, 'html.parser')
+
+        '''
+        print(data_link)
         headers={'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36',
                  'Cookie':"__cf_bm=vWFXMO.sZQjjMd_dcbHhr3BNG3J8oVMxL50SAp_J8gE-1708149530-1.0-AYR5232kdw5z8gcHkkUaaYLSMaZXh0RE4YGhiHb9gPmmUi8cXno4PnJQ2cPRfdWBEEK8kUDRnrOgSP9LAUJ+ieiCW6uRzYL/PAAjsaD08IQH"}
+                          # __cf_bm=BB3aw1nXKOryR_xthtnENBKFcuC0MPJiLeGOmRC_ddE-1708563134-1.0-AU7GCRk9oXfCgW82NKYmRYTab3WO4QnxUGu5Wyz7qAG52kJm003Bqiz0RqS8tQS/bSNhaQG8q1+iZGb6yByG/X0NrXseAa7lGl4WsZG5pm0
         r= requests.get(data_link, headers=headers)
-        data_dict= json.loads(r.content)
+        '''
+        
+        data_dict= json.loads(soup.find('body').contents[0])
         data_dict.pop('s', None)
         df= pd.DataFrame(data_dict)
         return df
@@ -71,9 +88,3 @@ class Stock:
         url= f'https://ngxgroup.com/exchange/data/company-profile/?symbol={upper_ticker}&directory=companydirectory'
         df= pd.read_html(url, match='Volume')[0]
         return df
-
-
-stock= Stock('DANGCEM')
-print(stock.getprice_experimental())
-#print(stock.trading_info())
-#print(stock.profile())
